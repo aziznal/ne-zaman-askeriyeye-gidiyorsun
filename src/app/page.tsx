10788e797a57bbc72042b49082bf7c37cf3de568
Abdulaziz Nal <abdulaziz.nal@anywai.ch>
2023-11-02 11:18:41 +0300

    setup initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,81 @@
-import Image from 'next/image'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  // TODO: use zustand with persistent storage?
+  const [locale, setLocale] = useState<"en" | "tr" | "de">("en");
+
+  const daysUntil9thNovember2023 = Math.ceil(
+    (new Date("2023-11-09T00:00:00.000Z").getTime() - Date.now()) /
+      (1000 * 60 * 60 * 24)
+  );
+
+  const daysUntil9thDecember2023 = Math.ceil(
+    (new Date("2023-12-09T00:00:00.000Z").getTime() - Date.now()) /
+      (1000 * 60 * 60 * 24)
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main
+      className="flex flex-col justify-center items-center h-[100vh] text-white"
+      style={{
+        background:
+          "linear-gradient(45deg, rgba(66,0,0,1) 0%, rgba(18,0,38,1) 57%, rgba(89,0,157,1) 100%)",
+      }}
+    >
+      <div className="flex gap-3">
+        <Button
+          variant={locale === "en" ? "secondary" : "default"}
+          onClick={() => setLocale("en")}
+        >
+          EN 🌎{" "}
+        </Button>
+
+        <Button
+          variant={locale === "tr" ? "secondary" : "default"}
+          onClick={() => setLocale("tr")}
+        >
+          TR 🇹🇷
+        </Button>
+
+        <Button
+          variant={locale === "de" ? "secondary" : "default"}
+          onClick={() => setLocale("de")}
+        >
+          DE 🇩🇪{" "}
+        </Button>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="text-center font-bold mt-12">
+        <h1 className="text-7xl">
+          {locale === "en" && "When am I going to my Military Service?"}
+          {locale === "tr" && "Askeriyeye ne zaman gidiyorum?"}
+          {locale === "de" && "Wann gehe ich zum Militärdienst?"}
+        </h1>
+
+        <h2 className="text-5xl mt-12 rainbow-text">
+          {locale === "en" &&
+            `9th November 2023 (${daysUntil9thNovember2023} days)`}
+          {locale === "tr" && `9. Kasım 2023 (${daysUntil9thNovember2023} gün)`}
+          {locale === "de" && `9. November 2023 (${daysUntil9thNovember2023} Tage)`}
+        </h2>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="text-center font-bold mt-24">
+        <h1 className="text-7xl">
+          {locale === "en" && "When am I coming back?"}
+          {locale === "tr" && "Ne zaman geri geliyorum?"}
+          {locale === "de" && "Wann komme ich zurück?"}
+        </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h2 className="text-5xl mt-12 rainbow-text">
+          {locale === "en" && `9th December 2023 (${daysUntil9thDecember2023} days)`}
+          {locale === "tr" && `9. Aralık 2023 (${daysUntil9thDecember2023} gün)`}
+          {locale === "de" && `9. Dezember 2023 (${daysUntil9thDecember2023} Tage)`}
+        </h2>
       </div>
     </main>
-  )
+  );
 }
