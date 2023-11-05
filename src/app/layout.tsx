96a2696ba51e9454604733a9fc463164b01e58ed
Abdulaziz Nal <abdulaziz.nal@anywai.ch>
2023-11-05 17:59:10 +0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         {children}
 
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
